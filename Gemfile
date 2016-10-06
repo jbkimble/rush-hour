e261c9a5390b4c65d3b00f28397fd49b143eb574
Ryan Travitz <rtravitz@gmail.com>
2016-10-06 17:15:41 -0600

    add payload table and model

--- Gemfile
+++ Gemfile
@@ -13,4 +13,8 @@ group :development, :test do
   gem 'capybara'
   gem 'launchy'
   gem 'rack-test'
+  gem 'pry'
+  gem 'rb-readline'
+  gem 'pry-coolline'
+  gem 'database_cleaner'
 end
---
+++ db/migrate/20161006230657_create_payloads.rb
@@ -0,0 +1,17 @@
+class CreatePayloads < ActiveRecord::Migration
+  def change
+    create_table  :payloads do |t|
+      t.string    :url
+      t.string    :requestedAt
+      t.integer   :respondedIn
+      t.string    :referredBy
+      t.string    :requestType
+      t.string    :eventName
+      t.string    :userAgent
+      t.string    :resolutionWidth
+      t.string    :resolutionHeight
+      t.string    :ip
+      t.timestamps null: false
+    end
+  end
+end
