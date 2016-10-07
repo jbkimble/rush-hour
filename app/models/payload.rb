56df0d0f6e4773cdd0989ef172b84ba135a5a1d1
Ryan Travitz <rtravitz@gmail.com>
2016-10-06 18:17:00 -0600

    finish iteration zero

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,3 +1,5 @@
 class Payload < ActiveRecord::Base
-  
+  validates :url, :requestedAt, :respondedIn, :referredBy, :requestType,
+            :eventName, :userAgent, :resolutionWidth, :resolutionHeight, :ip,
+            presence: true
 end
