ef917f13d1cbe33f2593eff22fede55284485666
Jon Kimble <jonathanbkimble@gmail.com>
2016-10-10 14:08:46 -0600

    finished iteration 4

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -3,7 +3,23 @@ module RushHour
     not_found do
       erb :error
     end
-    
-    # Write the rest of your controller code here!
+
+    post '/sources' do
+      @identifier = params[:identifier]
+      @root_url = params[:rootUrl]
+
+      if @identifier.nil? || @root_url.nil?
+        status 400
+        body "Missing Parameters - 400 Bad Request"
+      elsif Client.exists?(:identifier => @identifier)
+        status 403
+        body "#{@identifier} Already Exists - 403 Forbidden"
+      else
+        Client.create(identifier: @identifier, root_url: @root_url)
+        status 200
+        body "identifier: #{@identifier}"
+      end
+    end
+
   end
 end
